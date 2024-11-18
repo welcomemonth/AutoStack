597fb311b70c5e85dbadf4eab3b4bc5a4de8d9df
RecTechie <rexhub@163.com>
2024-11-18 21:34:14 +0800

    refactor(backend): 移除实体模块的导入并添加 Swagger依赖
    - 从 app.module.ts 中移除了 EntityModule 的导入- 在 package.json 中添加了 @nestjs/swagger 依赖

--- templates/backend/src/app.module.ts
+++ templates/backend/src/app.module.ts
@@ -4,14 +4,12 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { EntityModule } from './${entity_lower_underline}/$${entity_lower_underline}.module';
 
 @Module({
   imports: [
     UserModule,
     AuthModule,
     PrismaModule,
-    EntityModule
   ],
   exports: [],
   providers:[AppService],
